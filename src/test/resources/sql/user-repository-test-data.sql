5bf050410ea815a3f7267e1fed470320f539d5f5
나선재 <sky114z21@gmail.com>
2024-12-24 18:11:47 +0900

    UserRepository 서비스 코드 추가, 불필요한 코드 제거 및 리네이밍

---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+VALUES (1, 'test@test.com', 'sjna', 'Seoul', 'aaaaaaaaaaaaa-aaaaaaaa-aaaaa-aaaaaaa', 'ACTIVE', 0);
