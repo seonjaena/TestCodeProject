2c4e6c230be2f89e518af741fd32880afa841c03
나선재 <sky114z21@gmail.com>
2024-12-25 17:52:51 +0900

    PostCreateController에 테스트 코드 추가

--- src/test/java/com/example/demo/controller/PostCreateControllerTest.java
+++ src/test/java/com/example/demo/controller/PostCreateControllerTest.java
@@ -1,4 +1,62 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.dto.PostCreateDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "classpath:sql/post-create-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "classpath:sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
 public class PostCreateControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void 게시물을_작성할_수_있다() throws Exception {
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .writerId(1)
+                .content("create-post-test")
+                .build();
+
+        mockMvc.perform(post("/api/posts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(postCreateDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.content").value("create-post-test"))
+                .andExpect(jsonPath("$.writer.id").value(1));
+    }
+
+    @Test
+    void PENDING_상태의_사용자는_게시물을_작성할_수_없다() throws Exception {
+
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .writerId(2)
+                .content("failed-to-create-post")
+                .build();
+
+        mockMvc.perform(post("/api/posts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(postCreateDto)))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Users에서 ID 2를 찾을 수 없습니다."));
+    }
+
 }
---
+++ src/test/resources/sql/post-create-controller-test-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+VALUES (1, 'test@test.com', 'sjna', 'Seoul', 'aaaaaaaaaaaaa-aaaaaaaa-aaaaa-aaaaaaa', 'ACTIVE', 0);
+
+INSERT INTO `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+VALUES (2, 'test2@test.com', 'sjna2', 'Seoul', 'aaaaaaaaaaaaa-aaaaaaaa-aaaaa-aaaaaab', 'PENDING', 0);
+
